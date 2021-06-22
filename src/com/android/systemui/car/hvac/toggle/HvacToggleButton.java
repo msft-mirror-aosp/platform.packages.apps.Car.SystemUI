fa8b7f802dade5547004ab97eea9d8a26e4fc8ee
Heemin Seog <hseog@google.com>
2021-06-22 16:49:07 +0000

    Separate boolean and integer hvac toggle buttons
    Bug: 188595011
    Test: manual
    Change-Id: Ib898a2e57f55f00b915b4ed112ae888cfdaed889
    Merged-In: Ib898a2e57f55f00b915b4ed112ae888cfdaed889

--- src/com/android/systemui/car/hvac/toggle/HvacToggleButton.java
+++ src/com/android/systemui/car/hvac/toggle/HvacToggleButton.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.systemui.car.hvac;
+package com.android.systemui.car.hvac.toggle;
 
 import static android.car.VehiclePropertyIds.HVAC_AUTO_ON;
 import static android.car.VehiclePropertyIds.HVAC_POWER_ON;
@@ -24,13 +24,22 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.ImageButton;
 
-import com.android.systemui.R;
+import androidx.annotation.CallSuper;
 
-public class HvacToggleButton extends ImageButton implements HvacView {
-    private static int INVALID_ID = -1;
+import com.android.systemui.R;
+import com.android.systemui.car.hvac.HvacController;
+import com.android.systemui.car.hvac.HvacPropertySetter;
+import com.android.systemui.car.hvac.HvacView;
+
+/**
+ * An abstract toggle button which changes its drawable based off its state.
+ *
+ * @param <PropertyType> the type of the car property that is read/written by this toggle button.
+ */
+public abstract class HvacToggleButton<PropertyType> extends ImageButton implements HvacView {
+    private static final int INVALID_ID = -1;
 
     private int mPropertyId;
     private int mAreaId;
@@ -59,17 +68,68 @@ public class HvacToggleButton extends ImageButton implements HvacView {
         parseAttributes(attrs);
     }
 
+    public HvacToggleButton(Context context, AttributeSet attrs, int defStyleAttr,
+            int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        parseAttributes(attrs);
+    }
+
     @Override
-    public void onFinishInflate() {
+    protected void onFinishInflate() {
         super.onFinishInflate();
         setOnClickListener(v -> {
             if (!shouldAllowControl()) return;
 
             if (mHvacPropertySetter != null) {
-                mHvacPropertySetter.setHvacProperty(mPropertyId, mAreaId, /* value= */ !mIsOn);
+                handleClick(mHvacPropertySetter);
             }
         });
-        updateButtonView();
+        updateUIState();
+    }
+
+    @CallSuper
+    protected void parseAttributes(AttributeSet attrs) {
+        TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.HvacView);
+        mPropertyId = typedArray.getInt(R.styleable.HvacView_hvacPropertyId, INVALID_ID);
+        mAreaId = typedArray.getInt(R.styleable.HvacView_hvacAreaId, INVALID_ID);
+        mOnDrawable = typedArray.getDrawable(R.styleable.HvacView_hvacToggleOnButtonDrawable);
+        mOffDrawable = typedArray.getDrawable(R.styleable.HvacView_hvacToggleOffButtonDrawable);
+        mTurnOffIfPowerOff =
+                typedArray.getBoolean(R.styleable.HvacView_hvacTurnOffIfPowerOff, true);
+        mTurnOffIfAutoOn = typedArray.getBoolean(R.styleable.HvacView_hvacTurnOffIfAutoOn, false);
+        mOnAlpha = mContext.getResources().getFloat(R.dimen.hvac_turned_on_alpha);
+        mOffAlpha = mContext.getResources().getFloat(R.dimen.hvac_turned_off_alpha);
+        typedArray.recycle();
+    }
+
+    @CallSuper
+    protected void updateUIState() {
+        setAlpha(shouldAllowControl() ? mOnAlpha : mOffAlpha);
+        setImageDrawable(isToggleOn() ? mOnDrawable : mOffDrawable);
+    }
+
+    /** Handles when the toggle button is clicked. */
+    protected abstract void handleClick(HvacPropertySetter propertySetter);
+
+    /**
+     * Updates whether the toggle should be on or off. Returns the expected state given the property
+     * value.
+     */
+    protected abstract boolean updateToggleState(PropertyType propertyValue);
+
+    /** Returns whether the toggle is in the enabled state. */
+    protected abstract boolean isToggleOn();
+
+    protected boolean shouldAllowControl() {
+        if (mTurnOffIfPowerOff && !mPowerOn) {
+            return false;
+        }
+
+        if (mTurnOffIfAutoOn && mAutoOn) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
@@ -88,10 +148,10 @@ public class HvacToggleButton extends ImageButton implements HvacView {
         }
 
         if (value.getPropertyId() == getHvacPropertyToView()) {
-            mIsOn = (Boolean) value.getValue();
+            mIsOn = updateToggleState((PropertyType) value.getValue());
         }
 
-        updateButtonView();
+        updateUIState();
     }
 
     @Override
@@ -112,35 +172,5 @@ public class HvacToggleButton extends ImageButton implements HvacView {
     @Override
     public void onLocaleListChanged() {
         // no-op.
-    }
-
-    private void parseAttributes(AttributeSet attrs) {
-        TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.HvacView);
-        mPropertyId = typedArray.getInt(R.styleable.HvacView_hvacPropertyId, INVALID_ID);
-        mAreaId = typedArray.getInt(R.styleable.HvacView_hvacAreaId, INVALID_ID);
-        mOnDrawable = typedArray.getDrawable(R.styleable.HvacView_hvacToggleOnButtonDrawable);
-        mOffDrawable = typedArray.getDrawable(R.styleable.HvacView_hvacToggleOffButtonDrawable);
-        mTurnOffIfPowerOff =
-                typedArray.getBoolean(R.styleable.HvacView_hvacTurnOffIfPowerOff, true);
-        mTurnOffIfAutoOn = typedArray.getBoolean(R.styleable.HvacView_hvacTurnOffIfAutoOn, false);
-        mOnAlpha = mContext.getResources().getFloat(R.dimen.hvac_turned_on_alpha);
-        mOffAlpha = mContext.getResources().getFloat(R.dimen.hvac_turned_off_alpha);
-    }
-
-    private void updateButtonView() {
-        setAlpha(shouldAllowControl() ? mOnAlpha : mOffAlpha);
-        setImageDrawable(mIsOn ? mOnDrawable : mOffDrawable);
-    }
-
-    private boolean shouldAllowControl() {
-        if (mTurnOffIfPowerOff && !mPowerOn) {
-            return false;
-        }
-
-        if (mTurnOffIfAutoOn && mAutoOn) {
-            return false;
-        }
-
-        return true;
     }
 }

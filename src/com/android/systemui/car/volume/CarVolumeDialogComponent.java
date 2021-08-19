cc39eb7adb8b5a0d788b9efdbcdefac503dab59c
Caitlin Cassidy <ccassidy@google.com>
2021-08-19 18:47:19 +0000

    Revert "[Dagger] Remove CarVolumeDialogComponent now that the cu..."
    Revert "[Dagger] Add VolumeModule to SystemUIGoogleModule."
    
    Revert submission 15548780-dagger-volumedialogimpl
    
    Reason for revert: Breaking builds
    Reverted Changes:
    Iea3f06de8:[Dagger] Add VolumeModule to SystemUIGoogleModule....
    I3213f0c6f:[Dagger] Remove CarVolumeDialogComponent now that ...
    I74c71860b:[Dagger] Remove Dependency.get from VolumeDialogIm...
    
    Change-Id: I1e1df6b34e3c635600f4c1a876bb312850deab2f
    Bug: 138786270

---
+++ src/com/android/systemui/car/volume/CarVolumeDialogComponent.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.car.volume;
+
+import android.content.Context;
+
+import com.android.systemui.car.CarServiceProvider;
+import com.android.systemui.dagger.SysUISingleton;
+import com.android.systemui.demomode.DemoModeController;
+import com.android.systemui.keyguard.KeyguardViewMediator;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.PluginDependencyProvider;
+import com.android.systemui.plugins.VolumeDialog;
+import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.tuner.TunerService;
+import com.android.systemui.volume.VolumeDialogComponent;
+import com.android.systemui.volume.VolumeDialogControllerImpl;
+
+import javax.inject.Inject;
+
+/**
+ * Allows for adding car specific dialog when the volume dialog is created.
+ */
+@SysUISingleton
+public class CarVolumeDialogComponent extends VolumeDialogComponent {
+
+    private CarVolumeDialogImpl mCarVolumeDialog;
+
+    @Inject
+    public CarVolumeDialogComponent(Context context, KeyguardViewMediator keyguardViewMediator,
+            ActivityStarter activityStarter,
+            VolumeDialogControllerImpl volumeDialogController,
+            DemoModeController demoModeController,
+            PluginDependencyProvider pluginDependencyProvider,
+            ExtensionController extensionController,
+            TunerService tunerService,
+            CarServiceProvider carServiceProvider) {
+        super(
+                context,
+                keyguardViewMediator,
+                activityStarter,
+                volumeDialogController,
+                demoModeController,
+                pluginDependencyProvider,
+                extensionController,
+                tunerService);
+        mCarVolumeDialog.setCarServiceProvider(carServiceProvider);
+    }
+
+    /** This method is called while calling the super constructor. */
+    @Override
+    protected VolumeDialog createDefault() {
+        mCarVolumeDialog = new CarVolumeDialogImpl(mContext);
+        return mCarVolumeDialog;
+    }
+}

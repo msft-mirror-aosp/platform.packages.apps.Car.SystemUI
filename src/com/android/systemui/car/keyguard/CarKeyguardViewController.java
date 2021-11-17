ccd6d9424a1627120c565aa5eb02ec116a14f2b1
Yuncheol Heo <ycheo@google.com>
2021-11-16 17:54:14 -0800

    Disable the KeyGuard animation for unlock.
    To resolve TaskView misalignment issue after display off and on.
    
    Bug: 206869018
    Test: Check if Maps in TaskView is in position after display off and on.
    Change-Id: I8ca8ba274ecd300dde893122612f636fa00b6b7a

--- src/com/android/systemui/car/keyguard/CarKeyguardViewController.java
+++ src/com/android/systemui/car/keyguard/CarKeyguardViewController.java
@@ -318,7 +318,9 @@ public class CarKeyguardViewController extends OverlayViewController implements
 
     @Override
     public boolean shouldDisableWindowAnimationsForUnlock() {
-        return false;
+        // TODO(b/205189147): revert the following change after the proper fix is landed.
+        // Disables the KeyGuard animation to resolve TaskView misalignment issue after display-on.
+        return true;
     }
 
     @Override

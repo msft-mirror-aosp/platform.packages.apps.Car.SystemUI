d47da4985ddf135d35e420dd1d4881a8c1dcfaa6
Dave Mankoff <mankoff@google.com>
2022-07-08 18:20:20 +0000

    Revert^2 "Conform to new SystemUIInitializer pattern."
    deb6033f38cf0675c5b7521d0c30f51a8790213a
    
    Change-Id: I8b26f0b878aa215f953cff9b7d8ec7e2e3618549

--- tests/src/com/android/systemui/CarSystemUITestInitializer.java
+++ tests/src/com/android/systemui/CarSystemUITestInitializer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,36 +11,30 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License
+ * limitations under the License.
  */
 
 package com.android.systemui;
 
-import android.os.UserHandle;
+import static org.mockito.Mockito.mock;
 
-import com.android.systemui.dagger.GlobalRootComponent;
+import android.content.Context;
+
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.WMComponent;
-import com.android.systemui.wmshell.CarWMComponent;
+import com.android.wm.shell.RootTaskDisplayAreaOrganizer;
 
 import java.util.Optional;
 
-/**
- * Class factory to provide car specific SystemUI components.
- */
-public class CarSystemUIFactory extends SystemUIFactory {
-    @Override
-    protected GlobalRootComponent.Builder getGlobalRootComponentBuilder() {
-        return DaggerCarGlobalRootComponent.builder();
+public class CarSystemUITestInitializer extends CarSystemUIInitializer {
+    public CarSystemUITestInitializer(Context context) {
+        super(context);
     }
 
     @Override
     protected SysUIComponent.Builder prepareSysUIComponentBuilder(
             SysUIComponent.Builder sysUIBuilder, WMComponent wm) {
-        CarWMComponent carWm = (CarWMComponent) wm;
-        boolean isSystemUser = UserHandle.myUserId() == UserHandle.USER_SYSTEM;
         return ((CarSysUIComponent.Builder) sysUIBuilder).setRootTaskDisplayAreaOrganizer(
-                isSystemUser ? Optional.of(carWm.getRootTaskDisplayAreaOrganizer())
-                        : Optional.empty());
+                Optional.of(mock(RootTaskDisplayAreaOrganizer.class)));
     }
 }

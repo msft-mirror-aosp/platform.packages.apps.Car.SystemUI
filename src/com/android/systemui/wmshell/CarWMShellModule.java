eabb6b6f22a4bbb2ac89e5266e34fb73646caaf6
Ankit Shetgaonkar <ankiit@google.com>
2023-02-27 07:59:15 -0800

    Fixes the freezing issue when using Keyboard.
    -Adds a null check to invoke listener only when not null.
    -Change initializing the Controller for all MUMD Users to only Secondary user.
    Bug: 270760841.
    Test: Manual.
    
    Change-Id: I3915f97a59d4d9d2c5ac9754f415ff3d16c2003c

--- src/com/android/systemui/wmshell/CarWMShellModule.java
+++ src/com/android/systemui/wmshell/CarWMShellModule.java
@@ -58,7 +58,7 @@ public abstract class CarWMShellModule {
             DisplayImeController displayImeController,
             @Main Handler mainHandler,
             ShellInit shellInit) {
-        if (CarSystemUIUserUtil.isMUMDSystemUI()) {
+        if (CarSystemUIUserUtil.isSecondaryMUMDSystemUI()) {
             return Optional.of(
                     new MDSystemBarsController(displayImeController, mainHandler, shellInit));
         }

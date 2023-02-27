msft-mirror-aosp/platform.packages.apps.Car.SystemUI

commit 52f6933f0547cbc4f1cc15c56feed85675e74b12

Update DisplayInputLockInfoWindow UI
Bug: 262430932
Test: Manual

Change-Id: Icc3db84c17a998e8435f61bcdd6900f2ccd5f003

src/com/android/systemui/car/input/DisplayInputLockInfoWindow.java
@@ -37,6 +37,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.MainThread;
 
@@ -44,7 +45,7 @@ import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
 
 /**
- * A simple implementation to show a lock icon on the screen when user touches it,
+ * An implementation to show a lock icon on the screen when user touches it,
  * indicating display input lock is currently enabled.
  */
 public final class DisplayInputLockInfoWindow {
@@ -52,9 +53,9 @@ public final class DisplayInputLockInfoWindow {
     private final WindowManager mWindowManager;
     private final WindowManager.LayoutParams mLp;
     private final ViewGroup mLockInfoViewGroup;
-    private final View mLockIconView;
+    private TextView mLockIconView;
     private final long mDismissAnimDurationMs;
-    private final long mAutoDismissDelayMs;
+    private long mAutoDismissDelayMs;
 
     private boolean mIsShown;  // Only accessed from MainThread.
     private boolean mIsTransitioningToHide;  // Only accessed from MainThread.
@@ -76,7 +77,8 @@ public final class DisplayInputLockInfoWindow {
      * @param display The display to add the input lock info window on.
      */
     @MainThread
-    public DisplayInputLockInfoWindow(@NonNull Context context, @NonNull Display display) {
+    public DisplayInputLockInfoWindow(@NonNull Context context, @NonNull Display display,
+            int textResource, int configDismissDelay) {
         mHandler = context.getMainThreadHandler();
         // Construct an instance of WindowManager to add the input lock info window of
         // TYPE_SYSTEM_OVERLAY to the Display `display`.
@@ -85,14 +87,15 @@ public final class DisplayInputLockInfoWindow {
         mLockInfoViewGroup = (ViewGroup) LayoutInflater.from(context)
                 .inflate(R.layout.display_input_locked, /* root= */ null);
         mLockIconView = mLockInfoViewGroup.findViewById(R.id.display_input_lock_icon);
+        mLockIconView.setText(context.getString(textResource));
         mDismissAnimDurationMs = context.getResources().getInteger(
                 R.integer.config_displayInputLockIconAnimDuration);
         mAutoDismissDelayMs = context.getResources().getInteger(
-                R.integer.config_displayInputLockIconDismissDelay);
+                configDismissDelay);
         mLp = new WindowManager.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, TYPE_SYSTEM_OVERLAY,
                 FLAG_NOT_FOCUSABLE | FLAG_NOT_TOUCHABLE, PixelFormat.RGBA_8888);
 
-        mLp.gravity = Gravity.CENTER;
+        mLp.gravity = Gravity.BOTTOM;
         mLp.privateFlags |= SYSTEM_FLAG_SHOW_FOR_ALL_USERS;
         mLp.setTitle("DisplayInputLockInfo#" + display.getDisplayId());
     }
@@ -116,6 +119,25 @@ public final class DisplayInputLockInfoWindow {
     public void hide() {
         mHandler.removeCallbacks(mHideInputLockInfoRunnable);
         mHandler.post(mHideInputLockInfoRunnable);
+    }
+
+    /**
+     * Set the text resource to be displayed in the window
+     *
+     * @param newTextResource The int text resource for the window TextView
+     */
+    public void setText(@NonNull Context context, int newTextResource) {
+        mLockIconView.setText(context.getString(newTextResource));
+    }
+
+    /**
+     * Set the dismiss delay for the window
+     *
+     * @param newDismissDelay The duration until the window disappears
+     */
+    public void setDismissDelay(@NonNull Context context, int newDismissDelay) {
+        mAutoDismissDelayMs = context.getResources().getInteger(
+                newDismissDelay);
     }
 
     private void hideDelayed(long delayMillis) {

src/com/android/systemui/car/input/DisplayInputSinkController.java
@@ -38,6 +38,7 @@ import androidx.annotation.MainThread;
 import androidx.annotation.VisibleForTesting;
 
 import com.android.systemui.CoreStartable;
+import com.android.systemui.R;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Main;
 
@@ -212,7 +213,6 @@ public final class DisplayInputSinkController implements CoreStartable {
             Slog.w(TAG, "Unable to start display input lock: no valid display " + displayId);
             return;
         }
-
         if (isDisplayInputLockStarted(displayId)) {
             // Already started input lock for the given display.
             if (DBG) {
@@ -220,16 +220,27 @@ public final class DisplayInputSinkController implements CoreStartable {
             }
             return;
         }
-
+        Context uContext = mContext.createDisplayContext(display);
+        if (uContext == null) {
+            Slog.d(TAG, "User context to create DisplayInputLockInfoWindow is null");
+            uContext = mContext;
+        }
+        // Context needs to be effectively final
+        Context userContext = uContext;
         Slog.i(TAG, "Start input lock for display " + displayId);
 
         DisplayInputLockInfoWindow lockInfoWindow =
-                new DisplayInputLockInfoWindow(mContext, display);
+                new DisplayInputLockInfoWindow(userContext, display,
+                        R.string.display_input_lock_initial_text,
+                        R.integer.config_displayInputLockInitialDismissDelay);
         DisplayInputSink.OnInputEventListener callback = (event) -> {
             if (DBG) {
                 Slog.d(TAG, "Received input events while input is locked for display "
                         + event.getDisplayId());
             }
+            lockInfoWindow.setText(userContext, R.string.display_input_lock_text);
+            lockInfoWindow.setDismissDelay(userContext,
+                    R.integer.config_displayInputLockIconDismissDelay);
             lockInfoWindow.show();
         };
         mDisplayInputSinks.put(displayId, new DisplayInputSink(display, callback));

res/drawable/display_input_locked_background.xml
@@ -1,0 +1,21 @@
+<!--
+  ~ Copyright (C) 2023 The Android Open Source Project
+  ~
+  ~ Licensed under the Apache License, Version 2.0 (the "License");
+  ~ you may not use this file except in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~      http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing, software
+  ~ distributed under the License is distributed on an "AS IS" BASIS,
+  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  ~ See the License for the specific language governing permissions and
+  ~ limitations under the License.
+  -->
+
+<shape xmlns:android="http://schemas.android.com/apk/res/android"
+    android:shape="rectangle">
+    <solid android:color="@color/display_input_lock_background_color"/>
+    <corners android:radius="@dimen/display_input_lock_background_radius" />
+</shape>

res/drawable/ic_display_input_locked.xml
@@ -1,28 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
-  ~ Copyright (C) 2022 The Android Open Source Project
-  ~
-  ~ Licensed under the Apache License, Version 2.0 (the "License");
-  ~ you may not use this file except in compliance with the License.
-  ~ You may obtain a copy of the License at
-  ~
-  ~      http://www.apache.org/licenses/LICENSE-2.0
-  ~
-  ~ Unless required by applicable law or agreed to in writing, software
-  ~ distributed under the License is distributed on an "AS IS" BASIS,
-  ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  ~ See the License for the specific language governing permissions and
-  ~ limitations under the License.
-  -->
-
-<vector
-    xmlns:android="http://schemas.android.com/apk/res/android"
-    android:width="48dp"
-    android:height="48dp"
-    android:viewportHeight="48.0"
-    android:viewportWidth="48.0">
-    <path
-        android:fillColor="@android:color/white"
-        android:strokeColor="@android:color/black" android:strokeWidth="0.3"
-        android:pathData="M36,16h-2v-4c0,-5.52 -4.48,-10 -10,-10S14,6.48 14,12v4h-2c-2.21,0 -4,1.79 -4,4v20c0,2.21 1.79,4 4,4h24c2.21,0 4,-1.79 4,-4L40,20c0,-2.21 -1.79,-4 -4,-4zM24,34c-2.21,0 -4,-1.79 -4,-4s1.79,-4 4,-4 4,1.79 4,4 -1.79,4 -4,4zM30.2,16L17.8,16v-4c0,-3.42 2.78,-6.2 6.2,-6.2 3.42,0 6.2,2.78 6.2,6.2v4z"/>
-</vector>

res/layout/display_input_locked.xml
@@ -15,15 +15,22 @@
   ~ limitations under the License.
   -->
 
-<FrameLayout
+<LinearLayout
     xmlns:android="http://schemas.android.com/apk/res/android"
     android:layout_width="wrap_content"
-    android:layout_height="wrap_content">
+    android:layout_height="wrap_content"
+    android:paddingBottom="@dimen/display_input_lock_text_bottom_margin">
 
-    <ImageView
+    <TextView
         android:id="@+id/display_input_lock_icon"
-        android:layout_width="@dimen/display_input_lock_icon_width"
-        android:layout_height="@dimen/display_input_lock_icon_height"
-        android:layout_gravity="center"
-        android:src="@drawable/ic_display_input_locked" />
-</FrameLayout>
+        android:layout_width="@dimen/display_input_lock_text_width"
+        android:layout_height="wrap_content"
+        android:layout_gravity="center_vertical"
+        android:padding="@dimen/display_input_lock_text_padding"
+        android:background="@drawable/display_input_locked_background"
+        android:text="@string/display_input_lock_text"
+        android:textColor="@color/display_input_lock_icon_color"
+        android:textSize="@dimen/display_input_lock_text_size"
+        android:gravity="start"/>
+
+</LinearLayout>

res/values/colors.xml
@@ -133,4 +133,8 @@
 
     <!-- Color for clock text -->
     <color name="system_bar_clock_text_color">@*android:color/car_headline3</color>
+
+    <!-- Colors for display input lock icon and text -->
+    <color name="display_input_lock_icon_color">@android:color/white</color>
+    <color name="display_input_lock_background_color">@*android:color/car_grey_868</color>
 </resources>

res/values/config.xml
@@ -204,6 +204,8 @@
     <integer name="config_displayInputLockIconAnimDuration">250</integer>
     <!-- The number of milliseconds before the display input lock icon auto-dismisses. -->
     <integer name="config_displayInputLockIconDismissDelay">250</integer>
+    <!-- The number of milliseconds before the initial display input lock notification auto-dismisses. -->
+    <integer name="config_displayInputLockInitialDismissDelay">3000</integer>
 
     <!-- Determines whether the secondary user SystemUI must be overlaid -->
     <bool name="config_enableSecondaryUserRRO">false</bool>

res/values/dimens.xml
@@ -388,6 +388,9 @@
     <dimen name="fullscreen_indicator_width">80dp</dimen>
 
     <!-- Dimensions for Display Input Lock -->
-    <dimen name="display_input_lock_icon_width">415dp</dimen>
-    <dimen name="display_input_lock_icon_height">415dp</dimen>
+    <dimen name="display_input_lock_text_padding">32dp</dimen>
+    <dimen name="display_input_lock_text_bottom_margin">24dp</dimen>
+    <dimen name="display_input_lock_text_size">24sp</dimen>
+    <dimen name="display_input_lock_text_width">642dp</dimen>
+    <dimen name="display_input_lock_background_radius">8dp</dimen>
 </resources>

commit d8484f1864da34554a27314f3f9e67b6510589c3

Fix the broken DisplayInputSinkControllerTest
- The test was broken by commit 52f6933f0547cbc4f1cc15c56feed85675e74b12
- Refactors the creation of DisplayInputLockInfoWindow for stubbing.
- Moves the creation of the DisplayContext to DisplayInputLockWindow.

Bug: 270383677
Test: atest DisplayInputSinkControllerTest
Change-Id: I09965f793ee940390c4f4a14636a962ead9ef412

src/com/android/systemui/car/input/DisplayInputLockInfoWindow.java
@@ -82,8 +82,9 @@ public final class DisplayInputLockInfoWindow {
         mHandler = context.getMainThreadHandler();
         // Construct an instance of WindowManager to add the input lock info window of
         // TYPE_SYSTEM_OVERLAY to the Display `display`.
-        mWindowManager = context.createWindowContext(display, TYPE_SYSTEM_OVERLAY,
-                /* options= */ null).getSystemService(WindowManager.class);
+        mWindowManager = context.createDisplayContext(display)
+                .createWindowContext(TYPE_SYSTEM_OVERLAY, /* options= */ null)
+                .getSystemService(WindowManager.class);
         mLockInfoViewGroup = (ViewGroup) LayoutInflater.from(context)
                 .inflate(R.layout.display_input_locked, /* root= */ null);
         mLockIconView = mLockInfoViewGroup.findViewById(R.id.display_input_lock_icon);

src/com/android/systemui/car/input/DisplayInputSinkController.java
@@ -220,32 +220,28 @@ public final class DisplayInputSinkController implements CoreStartable {
             }
             return;
         }
-        Context uContext = mContext.createDisplayContext(display);
-        if (uContext == null) {
-            Slog.d(TAG, "User context to create DisplayInputLockInfoWindow is null");
-            uContext = mContext;
-        }
-        // Context needs to be effectively final
-        Context userContext = uContext;
         Slog.i(TAG, "Start input lock for display " + displayId);
-
-        DisplayInputLockInfoWindow lockInfoWindow =
-                new DisplayInputLockInfoWindow(userContext, display,
-                        R.string.display_input_lock_initial_text,
-                        R.integer.config_displayInputLockInitialDismissDelay);
+        DisplayInputLockInfoWindow lockInfoWindow = createDisplayInputLockInfoWindow(display);
         DisplayInputSink.OnInputEventListener callback = (event) -> {
             if (DBG) {
                 Slog.d(TAG, "Received input events while input is locked for display "
                         + event.getDisplayId());
             }
-            lockInfoWindow.setText(userContext, R.string.display_input_lock_text);
-            lockInfoWindow.setDismissDelay(userContext,
+            lockInfoWindow.setText(mContext, R.string.display_input_lock_text);
+            lockInfoWindow.setDismissDelay(mContext,
                     R.integer.config_displayInputLockIconDismissDelay);
             lockInfoWindow.show();
         };
         mDisplayInputSinks.put(displayId, new DisplayInputSink(display, callback));
         // Now that the display input lock is started, let's inform the user of it.
         lockInfoWindow.show();
+    }
+
+    @VisibleForTesting
+    DisplayInputLockInfoWindow createDisplayInputLockInfoWindow(Display display) {
+        return new DisplayInputLockInfoWindow(mContext, display,
+                        R.string.display_input_lock_initial_text,
+                        R.integer.config_displayInputLockInitialDismissDelay);
     }
 
     @VisibleForTesting

tests/src/com/android/systemui/car/input/DisplayInputSinkControllerTest.java
@@ -37,7 +37,6 @@ import android.content.ContentResolver;
 import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
 import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.UserHandle;
@@ -47,8 +46,6 @@ import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
 import android.view.Display;
 import android.view.InputEvent;
-import android.view.WindowManager;
-import android.window.WindowContext;
 
 import androidx.annotation.NonNull;
 import androidx.test.filters.SmallTest;
@@ -78,15 +75,10 @@ public class DisplayInputSinkControllerTest extends SysuiTestCase {
     @Mock
     private DisplayManager mDisplayManager;
     @Mock
-    private WindowContext mWindowContext;
-    @Mock
     private Display mDisplay;
-    @Mock
-    private WindowManager mWindowManager;
     @Mock
     private DisplayInputSink.OnInputEventListener mCallback;
 
-    private String mInitialSettingValue;
     private MockitoSession mMockingSession;
     private DisplayInputSinkController mDisplayInputSinkController;
     private Handler mHandler;
@@ -109,9 +101,6 @@ public class DisplayInputSinkControllerTest extends SysuiTestCase {
         mDisplayInputSinkController =
                 new DisplayInputSinkController(mContext, mHandler);
         spyOn(mDisplayInputSinkController);
-        doReturn(mWindowManager).when(mWindowContext).getSystemService(WindowManager.class);
-        doReturn(mWindowContext).when(mContext)
-                .createWindowContext(any(Display.class), anyInt(), any(Bundle.class));
         writeDisplayInputLockSetting(mContentResolver, EMPTY_SETTING_VALUE);
         doAnswer(invocation -> mDisplays.toArray(new Display[0]))
                 .when(mDisplayManager).getDisplays();
@@ -153,7 +142,7 @@ public class DisplayInputSinkControllerTest extends SysuiTestCase {
     @Test
     public void startDisplayInputLock_withValidDisplay_createsDisplayInputSink() {
         int displayId = 99;
-        doReturn(mDisplay).when(mDisplayManager).getDisplay(eq(displayId));
+        createDisplay(displayId, "testUniqueId");
 
         mDisplayInputSinkController.startDisplayInputLock(displayId);
 
@@ -213,11 +202,10 @@ public class DisplayInputSinkControllerTest extends SysuiTestCase {
     public void onDisplayAdded_withValidDisplay_callsStartDisplayInputLock() {
         int displayId = 99;
         String uniqueId = "testUniqueId";
+        createDisplay(displayId, uniqueId);
         doReturn(UserHandle.USER_SYSTEM).when(() -> UserHandle.myUserId());
         mDisplayInputSinkController.start();
         mDisplayInputSinkController.mDisplayInputLockSetting.add(uniqueId);
-        doReturn(mDisplay).when(mDisplayManager).getDisplay(eq(displayId));
-        doReturn(uniqueId).when(mDisplay).getUniqueId();
 
         mDisplayInputSinkController.mDisplayListener.onDisplayAdded(displayId);
 
@@ -376,15 +364,18 @@ public class DisplayInputSinkControllerTest extends SysuiTestCase {
 
     private Display createDisplay(int displayId, String uniqueId) {
         Display display = mock(Display.class);
+        doReturn(display).when(mDisplayManager).getDisplay(displayId);
         doReturn(uniqueId).when(display).getUniqueId();
         doReturn(displayId).when(display).getDisplayId();
         mDisplays.add(display);
+        doReturn(mock(DisplayInputLockInfoWindow.class))
+                .when(mDisplayInputSinkController).createDisplayInputLockInfoWindow(display);
         return display;
     }
 
     private void addDisplayInputSink(int displayId) {
         Display display = mock(Display.class);
-        doReturn(display).when(mDisplayManager).getDisplay(anyInt());
+        doReturn(display).when(mDisplayManager).getDisplay(displayId);
         DisplayInputSink displayInputSinks = new DisplayInputSink(display, mCallback);
         mDisplayInputSinkController.mDisplayInputSinks.put(displayId, displayInputSinks);
         assertThat(mDisplayInputSinkController.mDisplayInputSinks.get(displayId)).isNotNull();
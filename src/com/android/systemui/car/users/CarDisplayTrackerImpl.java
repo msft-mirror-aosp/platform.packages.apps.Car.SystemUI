3ca0d7f306d67b3bb87adc86613c29d10f2e5e78
Beverly <beverlyt@google.com>
2023-08-04 13:56:34 +0000

    Add `getDisplay` method to CarDisplayTrackerImpl
    Test: builds
    Bug: 290077758
    Change-Id: Ie07eadfd5e51d7f790b332b1a09479b5b9a910db

--- src/com/android/systemui/car/users/CarDisplayTrackerImpl.java
+++ src/com/android/systemui/car/users/CarDisplayTrackerImpl.java
@@ -155,6 +155,11 @@ public class CarDisplayTrackerImpl implements DisplayTracker {
     }
 
     @Override
+    public Display getDisplay(int displayId) {
+        return mDisplayManager.getDisplay(displayId);
+    }
+
+    @Override
     public void addDisplayChangeCallback(Callback callback, Executor executor) {
         synchronized (mDisplayCallbacks) {
             if (mDisplayCallbacks.isEmpty()) {

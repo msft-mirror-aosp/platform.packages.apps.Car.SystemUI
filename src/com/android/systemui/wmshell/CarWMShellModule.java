eff41a71fec7452c13407e46ebb4edcab607f0e0
Jainam Shah <jainams@google.com>
2025-03-20 15:17:15 -0700

    Add safe rectangle and caption bar to systemui
    - added safe region and auto decor
    - disabled caption bar using window decoration
    
    Bug: 405200165
    Test: manual
    Flag: com.android.systemui.car.display_compatibility_auto_decor_safe_region
    Change-Id: Ib42f13e5d5ae6e13785c0616e10e9cba5cac0a57

--- src/com/android/systemui/wmshell/CarWMShellModule.java
+++ src/com/android/systemui/wmshell/CarWMShellModule.java
@@ -27,6 +27,7 @@ import androidx.annotation.NonNull;
 
 import com.android.systemui.R;
 import com.android.systemui.car.CarServiceProvider;
+import com.android.systemui.car.wm.AutoCaptionPerDisplayInitializer;
 import com.android.systemui.car.wm.AutoDisplayCompatWindowDecorViewModel;
 import com.android.systemui.car.wm.CarFullscreenTaskMonitorListener;
 import com.android.systemui.car.wm.scalableui.PanelAutoTaskStackTransitionHandlerDelegate;
@@ -36,7 +37,9 @@ import com.android.systemui.car.wm.scalableui.panel.DecorPanel;
 import com.android.systemui.car.wm.scalableui.panel.TaskPanel;
 import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.wm.DisplaySystemBarsController;
+import com.android.wm.shell.RootTaskDisplayAreaOrganizer;
 import com.android.wm.shell.ShellTaskOrganizer;
+import com.android.wm.shell.automotive.AutoCaptionController;
 import com.android.wm.shell.automotive.AutoShellModule;
 import com.android.wm.shell.automotive.AutoTaskRepository;
 import com.android.wm.shell.common.DisplayController;
@@ -80,6 +83,19 @@ public abstract class CarWMShellModule {
             @Main Handler mainHandler) {
         return new DisplaySystemBarsController(context, wmService, displayController,
                 displayInsetsController, mainHandler);
+    }
+
+    @WMSingleton
+    @Provides
+    static Optional<AutoCaptionPerDisplayInitializer> provideAutoCaptionPerDisplayInitializer(
+            Context context,
+            ShellTaskOrganizer shellTaskOrganizer,
+            AutoCaptionController autoCaptionController,
+            DisplayController displayController,
+            RootTaskDisplayAreaOrganizer rootTaskDisplayAreaOrganizer) {
+        return Optional.of(
+                new AutoCaptionPerDisplayInitializer(context, shellTaskOrganizer,
+                        autoCaptionController, displayController, rootTaskDisplayAreaOrganizer));
     }
 
     @BindsOptionalOf

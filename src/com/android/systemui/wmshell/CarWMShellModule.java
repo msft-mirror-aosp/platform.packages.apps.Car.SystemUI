02e15dabd915603b2fcf74a5eb05742536a9d32e
Robert Gross <robertgross@google.com>
2025-06-26 11:14:53 +0000

    Enable Action config loader in ScalableUI library
    Bug: 423995605
    Test: atest CarSystemUITests
    Flag: com.android.systemui.car.scalable_ui_actions
    Change-Id: I8ce972037c4ae5f39dfd47bfe8c000106de9495c
    (cherry picked from commit faf30196111be4942aba0518226975e6ca7097a8)

--- src/com/android/systemui/wmshell/CarWMShellModule.java
+++ src/com/android/systemui/wmshell/CarWMShellModule.java
@@ -24,6 +24,7 @@ import com.android.systemui.R;
 import com.android.systemui.car.CarServiceProvider;
 import com.android.systemui.car.wm.AutoCaptionPerDisplayInitializer;
 import com.android.systemui.car.wm.CarFullscreenTaskMonitorListener;
+import com.android.systemui.car.wm.scalableui.ActionConfigReader;
 import com.android.systemui.car.wm.scalableui.PanelAutoTaskStackTransitionHandlerDelegate;
 import com.android.systemui.car.wm.scalableui.PanelConfigReader;
 import com.android.systemui.car.wm.scalableui.ScalableUIWMInitializer;
@@ -126,14 +127,24 @@ public abstract class CarWMShellModule {
 
     @WMSingleton
     @Provides
+    static Optional<ActionConfigReader> providesActionConfigReader(Context context) {
+        if (isScalableUIEnabled(context)) {
+            return Optional.of(new ActionConfigReader(context));
+        }
+        return Optional.empty();
+    }
+
+    @WMSingleton
+    @Provides
     static Optional<ScalableUIWMInitializer> provideScalableUIInitializer(ShellInit shellInit,
             Context context,
+            Optional<ActionConfigReader> actionConfigReaderOptional,
             Optional<PanelConfigReader> panelConfigReaderOptional,
             PanelAutoTaskStackTransitionHandlerDelegate delegate) {
         if (isScalableUIEnabled(context) && panelConfigReaderOptional.isPresent()) {
             return Optional.of(
-                    new ScalableUIWMInitializer(shellInit, panelConfigReaderOptional.get(),
-                            delegate));
+                    new ScalableUIWMInitializer(shellInit, actionConfigReaderOptional.get(),
+                            panelConfigReaderOptional.get(), delegate));
         }
         return Optional.empty();
     }

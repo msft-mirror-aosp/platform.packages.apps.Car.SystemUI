9a851824642909ba0e4be6f3c67c3abab7cec1ba
Abhijoy Saha <abhijoy@google.com>
2025-07-15 12:34:49 -0700

    Add provider for system UI windows & configuration
    This change also accounts for unavailable panels when creating
    SystemBarWindow & SystemBarConfiguration objects
    
    Fixes: 431859935
    Bug: 425434312
    Test: atest CarSystemUITests
    Flag: com.android.car.scalableui.enable_ext_panel_updates
    Change-Id: I3a25172fba4bd505c9338528e4519a94d1d28344

--- src/com/android/systemui/wmshell/CarWMComponent.java
+++ src/com/android/systemui/wmshell/CarWMComponent.java
@@ -16,19 +16,14 @@
 
 package com.android.systemui.wmshell;
 
-import static com.android.car.scalableui.loader.xml.SystemBarTagXmlParser.SYSTEM_BAR_PANEL_BOTTOM_ID;
-import static com.android.car.scalableui.loader.xml.SystemBarTagXmlParser.SYSTEM_BAR_PANEL_LEFT_ID;
-import static com.android.car.scalableui.loader.xml.SystemBarTagXmlParser.SYSTEM_BAR_PANEL_RIGHT_ID;
-import static com.android.car.scalableui.loader.xml.SystemBarTagXmlParser.SYSTEM_BAR_PANEL_TOP_ID;
-
 import com.android.systemui.car.wm.AutoCaptionPerDisplayInitializer;
 import com.android.systemui.car.wm.CarSystemUIProxyImpl;
 import com.android.systemui.car.wm.displayarea.DaViewTransitions;
 import com.android.systemui.car.wm.scalableui.EventDispatcher;
 import com.android.systemui.car.wm.scalableui.ScalableUIWMInitializer;
 import com.android.systemui.car.wm.scalableui.panel.TaskPanelInfoRepository;
-import com.android.systemui.car.wm.scalableui.systemwindow.SystemBarWindow;
-import com.android.systemui.car.wm.scalableui.systemwindow.SystemBarWindow.SystemBarConfiguration;
+import com.android.systemui.car.wm.scalableui.systemwindow.SystemUiConfigurationProvider;
+import com.android.systemui.car.wm.scalableui.systemwindow.SystemUiWindowProvider;
 import com.android.systemui.car.wm.taskview.RemoteCarTaskViewTransitions;
 import com.android.systemui.wm.DisplaySystemBarsController;
 import com.android.wm.shell.RootTaskDisplayAreaOrganizer;
@@ -41,8 +36,6 @@ import com.android.wm.shell.dagger.WMSingleton;
 import dagger.Subcomponent;
 
 import java.util.Optional;
-
-import javax.inject.Named;
 
 /**
  * Dagger Subcomponent for WindowManager.
@@ -117,43 +110,11 @@ public interface CarWMComponent extends WMComponent {
     @WMSingleton
     AutoLayoutManager getAutoLayoutManager();
 
-    /** Provides the {@link SystemBarWindow} for the left side  */
+    /** Provides the {@link SystemUiWindowProvider} */
     @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_LEFT_ID)
-    Optional<SystemBarWindow> getLeftSystemBarWindow();
+    SystemUiWindowProvider getSystemUiWindowProvider();
 
-    /** Provides the {@link SystemBarWindow} for the top side  */
+    /** Provides the {@link SystemUiConfigurationProvider} */
     @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_TOP_ID)
-    Optional<SystemBarWindow> getTopSystemBarWindow();
-
-    /** Provides the {@link SystemBarWindow} for the right side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_RIGHT_ID)
-    Optional<SystemBarWindow> getRightSystemBarWindow();
-
-    /** Provides the {@link SystemBarWindow} for the bottom side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_BOTTOM_ID)
-    Optional<SystemBarWindow> getBottomSystemBarWindow();
-
-    /** Provides the {@link SystemBarConfiguration} for the left side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_LEFT_ID)
-    Optional<SystemBarConfiguration> getLeftSystemBarConfiguration();
-
-    /** Provides the {@link SystemBarConfiguration} for the top side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_TOP_ID)
-    Optional<SystemBarConfiguration> getTopSystemBarConfiguration();
-
-    /** Provides the {@link SystemBarConfiguration} for the right side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_RIGHT_ID)
-    Optional<SystemBarConfiguration> getRightSystemBarConfiguration();
-
-    /** Provides the {@link SystemBarConfiguration} for the bottom side  */
-    @WMSingleton
-    @Named(SYSTEM_BAR_PANEL_BOTTOM_ID)
-    Optional<SystemBarConfiguration> getBottomSystemBarConfiguration();
+    SystemUiConfigurationProvider getSystemUiConfigurationProvider();
 }
--- src/com/android/systemui/wmshell/CarWMShellModule.java
+++ src/com/android/systemui/wmshell/CarWMShellModule.java
@@ -287,8 +287,13 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarWindow> provideLeftSystemBarWindow(Context context,
             Optional<PanelUpdateConsumer> consumer, EventDispatcher dispatcher) {
         if (consumer.isPresent()) {
-            return Optional.of(
-                    new SystemBarWindow(context, consumer, dispatcher, SYSTEM_BAR_PANEL_LEFT_ID));
+            try {
+                return Optional.of(
+                        new SystemBarWindow(context, consumer, dispatcher,
+                                SYSTEM_BAR_PANEL_LEFT_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -299,8 +304,13 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarWindow> provideTopSystemBarWindow(Context context,
             Optional<PanelUpdateConsumer> consumer, EventDispatcher dispatcher) {
         if (consumer.isPresent()) {
-            return Optional.of(
-                    new SystemBarWindow(context, consumer, dispatcher, SYSTEM_BAR_PANEL_TOP_ID));
+            try {
+                return Optional.of(
+                        new SystemBarWindow(context, consumer, dispatcher,
+                                SYSTEM_BAR_PANEL_TOP_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -311,8 +321,13 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarWindow> provideRightSystemBarWindow(Context context,
             Optional<PanelUpdateConsumer> consumer, EventDispatcher dispatcher) {
         if (consumer.isPresent()) {
-            return Optional.of(
-                    new SystemBarWindow(context, consumer, dispatcher, SYSTEM_BAR_PANEL_RIGHT_ID));
+            try {
+                return Optional.of(
+                        new SystemBarWindow(context, consumer, dispatcher,
+                                SYSTEM_BAR_PANEL_RIGHT_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -323,8 +338,13 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarWindow> provideBottomSystemBarWindow(Context context,
             Optional<PanelUpdateConsumer> consumer, EventDispatcher dispatcher) {
         if (consumer.isPresent()) {
-            return Optional.of(
-                    new SystemBarWindow(context, consumer, dispatcher, SYSTEM_BAR_PANEL_BOTTOM_ID));
+            try {
+                return Optional.of(
+                        new SystemBarWindow(context, consumer, dispatcher,
+                                SYSTEM_BAR_PANEL_BOTTOM_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -335,7 +355,11 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarConfiguration> provideLeftSystemBarConfiguration(
             Optional<PanelUpdateConsumer> consumer) {
         if (consumer.isPresent()) {
-            return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_LEFT_ID));
+            try {
+                return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_LEFT_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -346,7 +370,11 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarConfiguration> provideTopSystemBarConfiguration(
             Optional<PanelUpdateConsumer> consumer) {
         if (consumer.isPresent()) {
-            return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_TOP_ID));
+            try {
+                return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_TOP_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -357,7 +385,11 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarConfiguration> provideRightSystemBarConfiguration(
             Optional<PanelUpdateConsumer> consumer) {
         if (consumer.isPresent()) {
-            return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_RIGHT_ID));
+            try {
+                return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_RIGHT_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }
@@ -368,7 +400,12 @@ public abstract class CarWMShellModule {
     static Optional<SystemBarConfiguration> provideBottomSystemBarConfiguration(
             Optional<PanelUpdateConsumer> consumer) {
         if (consumer.isPresent()) {
-            return Optional.of(new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_BOTTOM_ID));
+            try {
+                return Optional.of(
+                        new SystemBarConfiguration(consumer, SYSTEM_BAR_PANEL_BOTTOM_ID));
+            } catch (IllegalStateException e) {
+                return Optional.empty();
+            }
         }
         return Optional.empty();
     }

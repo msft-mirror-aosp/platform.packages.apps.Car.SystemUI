575133a4e584a4e3dc56ec9bcc3889aeecfe84ea
Alex Stetson <alexstetson@google.com>
2025-07-16 12:00:32 +0000

    Launch home intent on user unlock
    Home must be guaranteed as a visibility barrier in ScalableUI. However
    depending on the timing of initialization, it may not be initially
    present on boot. Explicitly starting home activity until a dedicated
    visibility barrier is ready.
    
    Bug: 406746950
    Test: manual
    Flag: NONE bugfix
    Change-Id: I1dd9c3663e59846b9dd0ab7930e4fafe3a7cf9ae

--- src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
+++ src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
@@ -26,6 +26,7 @@ import static com.android.wm.shell.Flags.enableAutoTaskStackController;
 
 import android.car.user.CarUserManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.Log;
@@ -96,6 +97,12 @@ public class SystemEventHandler implements CoreStartable,
 
                     if (event.getEventType() == USER_LIFECYCLE_EVENT_TYPE_UNLOCKED) {
                         if (event.getUserId() == mUserTracker.getUserId()) {
+                            // TODO(b/432217693): remove once visibility barrier is not home
+                            Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                            homeIntent.addCategory(Intent.CATEGORY_HOME);
+                            homeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivityAsUser(homeIntent, mUserTracker.getUserHandle());
+
                             StateManager.handlePanelReset();
                         } else {
                             Log.i(TAG, "Not current user" + event.getUserId());

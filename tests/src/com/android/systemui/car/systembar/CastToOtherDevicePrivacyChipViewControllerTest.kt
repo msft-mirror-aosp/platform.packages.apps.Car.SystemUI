12f82c1968568bf03c2ef182c690c7c6a8150250
Joy Yoonhyung Lee <yooonlee@google.com>
2025-08-28 15:42:38 +0900

    Add app sharing and cast privacy chip controllers
    Create view controllers for app sharing and cast indicators.
    The indicators will be located in the status bar as privacy chips and
    the controllers show or hide chips by leveraging OngoingActivityChipViewModel which was implemented by AAOS SystemUI.
    
    Bug: 435279151
    Test: atest CarSystemUITests:com.android.systemui.car.systembar.ShareToAppPrivacyChipViewControllerTest
    Flag: com.android.systemui.car.show_media_projection_indicator
    Change-Id: I0348e379287024b9687dbfa1077086a4d37a66da

---
+++ tests/src/com/android/systemui/car/systembar/CastToOtherDevicePrivacyChipViewControllerTest.kt
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.car.systembar
+
+import android.platform.test.annotations.EnableFlags
+import android.platform.test.flag.junit.SetFlagsRule
+import android.testing.AndroidTestingRunner
+import android.testing.TestableLooper.RunWithLooper
+import androidx.test.filters.SmallTest
+import com.android.internal.logging.InstanceId
+import com.android.systemui.CarSysuiTestCase
+import com.android.systemui.car.CarSystemUiTest
+import com.android.systemui.car.Flags.FLAG_SHOW_MEDIA_PROJECTION_INDICATOR
+import com.android.systemui.car.privacy.PrivacyChip
+import com.android.systemui.car.systembar.element.CarSystemBarElementStateController
+import com.android.systemui.car.systembar.element.CarSystemBarElementStatusBarDisableController
+import com.android.systemui.statusbar.chips.casttootherdevice.ui.viewmodel.CastToOtherDeviceChipViewModel
+import com.android.systemui.statusbar.chips.ui.model.ColorsModel
+import com.android.systemui.statusbar.chips.ui.model.OngoingActivityChipModel
+import java.util.concurrent.Executor
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.any
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.stub
+import org.mockito.kotlin.verify
+
+@CarSystemUiTest
+@RunWith(AndroidTestingRunner::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+@EnableFlags(FLAG_SHOW_MEDIA_PROJECTION_INDICATOR)
+@RunWithLooper
+@SmallTest
+class CastToOtherDevicePrivacyChipViewControllerTest : CarSysuiTestCase() {
+
+    @get:Rule val setFlagsRule = SetFlagsRule()
+
+    private val testScope = TestScope()
+    private val runnableArgumentCaptor = argumentCaptor<Runnable>()
+    private val chipModelFlow: MutableStateFlow<OngoingActivityChipModel> =
+        MutableStateFlow(OngoingActivityChipModel.Inactive())
+    private val executor = mock<Executor>()
+    private val instanceId = mock<InstanceId>()
+    private val privacyChip = mock<PrivacyChip>()
+    private val barElementDisableController = mock<CarSystemBarElementStatusBarDisableController>()
+    private val barElementStateController = mock<CarSystemBarElementStateController>()
+    private val castToOtherDeviceChipViewModel =
+        mock<CastToOtherDeviceChipViewModel> { on { chip } doReturn chipModelFlow }
+
+    private lateinit var castToOtherDevicePrivacyChipViewController:
+        CastToOtherDevicePrivacyChipViewController
+
+    @Before
+    fun setUp() {
+        val context = spy(mContext).stub { on { mainExecutor } doReturn executor }
+        castToOtherDevicePrivacyChipViewController =
+            CastToOtherDevicePrivacyChipViewController(
+                privacyChip,
+                barElementDisableController,
+                barElementStateController,
+                context,
+                testScope.backgroundScope,
+                castToOtherDeviceChipViewModel,
+            )
+    }
+
+    @Test
+    fun onViewAttached_callsAnimateIn_whenStateIsActive() =
+        testScope.runTest {
+            castToOtherDevicePrivacyChipViewController.onViewAttachedInternal()
+
+            chipModelFlow.value = createActiveChipModel()
+            runCurrent()
+            verify(executor).execute(runnableArgumentCaptor.capture())
+            runnableArgumentCaptor.firstValue.run()
+
+            verify(privacyChip).animateIn()
+        }
+
+    @Test
+    fun onViewAttached_callsAnimateOut_whenStateIsInactive() =
+        testScope.runTest {
+            castToOtherDevicePrivacyChipViewController.onViewAttachedInternal()
+
+            chipModelFlow.value = OngoingActivityChipModel.Inactive()
+            runCurrent()
+            verify(executor).execute(runnableArgumentCaptor.capture())
+            runnableArgumentCaptor.firstValue.run()
+
+            verify(privacyChip).animateOut()
+        }
+
+    @Test
+    fun onViewDetached_stopsCollectingState() =
+        testScope.runTest {
+            castToOtherDevicePrivacyChipViewController.onViewDetachedInternal()
+
+            chipModelFlow.value = createActiveChipModel()
+            runCurrent()
+
+            verify(executor, never()).execute(any())
+        }
+
+    private fun createActiveChipModel() =
+        OngoingActivityChipModel.Active(
+            key = "CastToOtherDevice",
+            icon = null,
+            content = OngoingActivityChipModel.Content.Text("CastToOtherDevice"),
+            colors = ColorsModel.Red,
+            onClickListenerLegacy = null,
+            clickBehavior = OngoingActivityChipModel.ClickBehavior.None,
+            instanceId = instanceId,
+        )
+}

9c521f1c2235699667a7f4895849802c5f8e3307
Alex Stetson <alexstetson@google.com>
2025-09-10 13:02:38 -0700

    Add config to skip sending events for specific components
    For certain activity launches, it may be desired to skip sending events
    that may affect the state of the panels. A specific config is
    provided for this use case.
    
    Bug: 441242168
    Test: manual
    Flag: NONE bugfix
    Change-Id: Ib7ace345e581f94e9c2de7e1f4f0e9c2668355ef

--- tests/src/com/android/systemui/car/wm/scalableui/PanelAutoTaskStackTransitionHandlerDelegateTest.java
+++ tests/src/com/android/systemui/car/wm/scalableui/PanelAutoTaskStackTransitionHandlerDelegateTest.java
@@ -73,6 +73,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
 
     private static final String TEST_PANEL_ID = "test_panel";
     private static final String TEST_COMPONENT_NAME = "com.test/com.test.TestActivity";
+    private static final int TEST_ROOT_TASK_ID = 100;
 
     private PanelAutoTaskStackTransitionHandlerDelegate mDelegate;
 
@@ -98,6 +99,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         MockitoAnnotations.initMocks(this);
         when(mPanelTransitionCoordinator.createAutoTaskStackTransaction(any(),
                 any(), any())).thenReturn(new AutoTaskStackTransaction());
+        when(mPanelUtils.handles(TEST_ROOT_TASK_ID)).thenReturn(true);
         mDelegate = new PanelAutoTaskStackTransitionHandlerDelegate(mContext,
                 mAutoTaskStackController, mPanelTransitionCoordinator, mPanelUtils,
                 mCarWMUserHelper, mTaskPanelInfoRepository, mAutoLayoutManager, mFlagManager);
@@ -210,6 +212,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         ActivityManager.RunningTaskInfo taskInfo = new ActivityManager.RunningTaskInfo();
         taskInfo.baseIntent = new Intent();
         taskInfo.baseIntent.addCategory(Intent.CATEGORY_HOME);
+        taskInfo.topActivityType = ACTIVITY_TYPE_HOME;
         when(request.getType()).thenReturn(TRANSIT_OPEN);
         when(request.getTriggerTask()).thenReturn(taskInfo);
 
@@ -223,6 +226,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         TransitionRequestInfo request = mock(TransitionRequestInfo.class);
         ActivityManager.RunningTaskInfo taskInfo = new ActivityManager.RunningTaskInfo();
         taskInfo.baseIntent = new Intent();
+        taskInfo.parentTaskId = TEST_ROOT_TASK_ID;
         when(request.getType()).thenReturn(TRANSIT_OPEN);
         when(request.getTriggerTask()).thenReturn(taskInfo);
         when(request.getFlags()).thenReturn(WindowManager.TRANSIT_FLAG_AVOID_MOVE_TO_FRONT);
@@ -239,6 +243,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         TaskPanel panel = mock(TaskPanel.class);
         ActivityManager.RunningTaskInfo taskInfo = new ActivityManager.RunningTaskInfo();
         taskInfo.baseIntent = new Intent();
+        taskInfo.parentTaskId = TEST_ROOT_TASK_ID;
         when(request.getType()).thenReturn(TRANSIT_OPEN);
         when(request.getTriggerTask()).thenReturn(taskInfo);
         ComponentName componentName = ComponentName.unflattenFromString(TEST_COMPONENT_NAME);
@@ -259,6 +264,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         TaskPanel panel = mock(TaskPanel.class);
         ActivityManager.RunningTaskInfo taskInfo = new ActivityManager.RunningTaskInfo();
         taskInfo.baseIntent = new Intent();
+        taskInfo.parentTaskId = TEST_ROOT_TASK_ID;
         when(request.getType()).thenReturn(TRANSIT_CLOSE);
         when(request.getTriggerTask()).thenReturn(taskInfo);
         ComponentName componentName = ComponentName.unflattenFromString(TEST_COMPONENT_NAME);
@@ -278,6 +284,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegateTest extends CarSysuiTes
         TransitionRequestInfo request = mock(TransitionRequestInfo.class);
         ActivityManager.RunningTaskInfo taskInfo = new ActivityManager.RunningTaskInfo();
         taskInfo.baseIntent = new Intent();
+        taskInfo.parentTaskId = TEST_ROOT_TASK_ID;
         when(request.getType()).thenReturn(TRANSIT_CLOSE);
         when(request.getTriggerTask()).thenReturn(taskInfo);
         ComponentName componentName = ComponentName.unflattenFromString(TEST_COMPONENT_NAME);

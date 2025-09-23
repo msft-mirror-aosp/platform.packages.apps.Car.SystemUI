ec0cec81a873fed2ecca7d2bfeb6461c5caaf458
Alex Stetson <alexstetson@google.com>
2025-09-23 11:19:31 -0700

    Use consistent data source for keyguard visibility
    The KeyguardManager uses isKeyguardShowing internally for its callback
    while the initial state we can get is isKeyguardLocked. This makes the
    diff in states sometimes inconsistent. Using an alternative data source
    that may be more consistent can help with reliability.
    
    Bug: 432178482
    Test: manual
    Flag: NONE bugfix
    Change-Id: I273dd39da23592c4aa886eee9181593ea84e410b

--- src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
+++ src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
@@ -63,6 +63,7 @@ import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.settings.DisplayTracker;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import dagger.Lazy;
 
@@ -94,7 +95,7 @@ public class SystemEventHandler implements CoreStartable,
     private final UserTracker mUserTracker;
     private final DisplayTracker mDisplayTracker;
     private final Lazy<DisplayStateHelper> mDisplayStateHelper;
-    private final KeyguardManager mKeyguardManager;
+    private final KeyguardStateController mKeyguardStateController;
     private final Executor mBackgroundExecutor;
     private final CarDeviceProvisionedController mCarDeviceProvisionedController;
     private final EventDispatcher mEventDispatcher;
@@ -141,12 +142,12 @@ public class SystemEventHandler implements CoreStartable,
 
                             StateManager.handlePanelReset();
                             mResetCalledForUser = true;
-
-                            if (!mIsKeyguardShowing) {
-                                sendUserAuthEvent();
-                            }
                         } else {
                             Log.d(TAG, "Received user unlock while user is not setup");
+                        }
+
+                        if (!mIsKeyguardShowing) {
+                            sendUserAuthEvent();
                         }
                     } else {
                         Log.i(TAG, "Ignore system event" + event.getEventType());
@@ -209,25 +210,6 @@ public class SystemEventHandler implements CoreStartable,
                 }
             };
 
-    private final KeyguardManager.KeyguardLockedStateListener mKeyguardListener =
-            new KeyguardManager.KeyguardLockedStateListener() {
-                @Override
-                public void onKeyguardLockedStateChanged(boolean isKeyguardLocked) {
-                    if (mIsKeyguardShowing == isKeyguardLocked) {
-                        return;
-                    }
-                    mIsKeyguardShowing = isKeyguardLocked;
-                    if (mIsKeyguardShowing) {
-                        sendEvent(new Event.Builder(SYSTEM_KEYGUARD_SHOWN_EVENT_ID));
-                    } else {
-                        sendEvent(new Event.Builder(SYSTEM_KEYGUARD_HIDDEN_EVENT_ID));
-                        if (mUserManager.isUserUnlocked(mUserTracker.getUserId())) {
-                            sendUserAuthEvent();
-                        }
-                    }
-                }
-            };
-
     @Inject
     public SystemEventHandler(
             Context context,
@@ -237,6 +219,7 @@ public class SystemEventHandler implements CoreStartable,
             UserTracker userTracker,
             DisplayTracker displayTracker,
             Lazy<DisplayStateHelper> displayStateHelper,
+            KeyguardStateController keyguardStateController,
             KeyguardManager keyguardManager,
             CarDeviceProvisionedController carDeviceProvisionedController,
             EventDispatcher dispatcher,
@@ -249,7 +232,7 @@ public class SystemEventHandler implements CoreStartable,
         mUserTracker = userTracker;
         mDisplayTracker = displayTracker;
         mDisplayStateHelper = displayStateHelper;
-        mKeyguardManager = keyguardManager;
+        mKeyguardStateController = keyguardStateController;
         mCarDeviceProvisionedController = carDeviceProvisionedController;
         mEventDispatcher = dispatcher;
         mFlagManager = flagManager;
@@ -277,8 +260,7 @@ public class SystemEventHandler implements CoreStartable,
             registerProvisionedStateListener();
             mUserTracker.addCallback(mUserTrackerCallback, mBackgroundExecutor);
             mDisplayStateHelper.get().addListener(mDisplayStateListener);
-            mKeyguardManager.addKeyguardLockedStateListener(mBackgroundExecutor, mKeyguardListener);
-            mIsKeyguardShowing = mKeyguardManager.isKeyguardLocked();
+            registerKeyguardStateListener();
         }
     }
 
@@ -321,6 +303,35 @@ public class SystemEventHandler implements CoreStartable,
                 mCarUserManager.addListener(mBackgroundExecutor, mUserLifecycleListener);
             }
         });
+    }
+
+    private void registerKeyguardStateListener() {
+        mIsKeyguardShowing = isKeyguardShowing();
+        mKeyguardStateController.addCallback(new KeyguardStateController.Callback() {
+            @Override
+            public void onKeyguardShowingChanged() {
+                keyguardShowingChanged(isKeyguardShowing());
+            }
+        });
+    }
+
+    private void keyguardShowingChanged(boolean showing) {
+        if (mIsKeyguardShowing == showing) {
+            return;
+        }
+        mIsKeyguardShowing = showing;
+        if (mIsKeyguardShowing) {
+            sendEvent(new Event.Builder(SYSTEM_KEYGUARD_SHOWN_EVENT_ID));
+        } else {
+            sendEvent(new Event.Builder(SYSTEM_KEYGUARD_HIDDEN_EVENT_ID));
+            if (mUserManager.isUserUnlocked(mUserTracker.getUserId())) {
+                sendUserAuthEvent();
+            }
+        }
+    }
+
+    private boolean isKeyguardShowing() {
+        return mKeyguardStateController.isShowing();
     }
 
     private boolean shouldResetPanels() {

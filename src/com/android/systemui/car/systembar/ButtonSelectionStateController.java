1c6f47581711a29fcbfc0c9f6c96fe949678d0fe
Alex Stetson <alexstetson@google.com>
2025-09-25 14:27:09 -0700

    Add separate configuration to enable ScalableUI
    When switching over to the FlagManager, this config was removed in favor
    of flags. However ScalableUI is not a mandatory feature and should have
    a separate config (not flag) to toggle its enablement.
    
    Bug: 446923475
    Test: manual (on ScalableUI and non-ScalableUI targets)
    Flag: NONE bugfix
    Change-Id: I85b6bd1f7d3493f0e0d4e49f54fbb306c76128c9
    (cherry picked from commit a35f8f7c098a4eca555cba521f330f0245b8ad6c)

--- src/com/android/systemui/car/systembar/ButtonSelectionStateController.java
+++ src/com/android/systemui/car/systembar/ButtonSelectionStateController.java
@@ -41,6 +41,7 @@ import com.android.car.scalableui.manager.StateManager;
 import com.android.car.scalableui.model.PanelState;
 import com.android.systemui.car.flags.Flag;
 import com.android.systemui.car.flags.FlagManager;
+import com.android.systemui.car.wm.scalableui.ScalableUIUtils;
 import com.android.systemui.car.wm.scalableui.panel.TaskPanelInfoRepository;
 import com.android.systemui.dagger.SysUISingleton;
 
@@ -114,7 +115,8 @@ public class ButtonSelectionStateController {
         mSelectedButtonsForPanelApp = new HashSet<>();
         mSelectedButtonsForPanelVisibility = new HashSet<>();
         mFlagManager = flagManager;
-        if (isScalableUIEnabled() && mTaskPanelInfoRepository != null) {
+        if (ScalableUIUtils.isScalableUIEnabled(mContext, mFlagManager)
+                && mTaskPanelInfoRepository != null) {
             mTaskPanelInfoRepository.addChangeListener(mTaskPanelListener);
             StateManager.getInstance().addPanelStateObserver(mPanelStateObserver);
         }
@@ -171,7 +173,7 @@ public class ButtonSelectionStateController {
      * @param validDisplay index of the valid display
      */
     protected void taskChanged(List<RootTaskInfo> taskInfoList, int validDisplay) {
-        if (isScalableUIEnabled()) {
+        if (ScalableUIUtils.isScalableUIEnabled(mContext, mFlagManager)) {
             return;
         }
         RootTaskInfo validTaskInfo = null;
@@ -402,7 +404,8 @@ public class ButtonSelectionStateController {
             }
         });
 
-        if (isScalableUIEnabled() && mTaskPanelInfoRepository != null) {
+        if (ScalableUIUtils.isScalableUIEnabled(mContext, mFlagManager)
+                && mTaskPanelInfoRepository != null) {
             if (carSystemBarButton.isAttachedToWindow()) {
                 selectForInitialPanelTaskState(carSystemBarButton);
             } else {
@@ -495,10 +498,6 @@ public class ButtonSelectionStateController {
             }
         }
         return null;
-    }
-
-    private boolean isScalableUIEnabled() {
-        return mFlagManager.isEnabled(Flag.ScalableUIEnabled);
     }
 
     // simple multi-map

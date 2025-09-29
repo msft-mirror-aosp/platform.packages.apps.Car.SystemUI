73995147167f1ec544b949b254bb1d74c272e675
Calvin Huang <calhuang@google.com>
2025-09-29 23:24:40 +0000

    Remove outdated reload logic
    - reload logic moved to ScalableUIWMInitializer
    
    Bug: 446745283
    Test: manual
    Flag: com.android.car.scalableui.scalable_ui_handle_configuration_change
    Change-Id: Ic840c03b64628a59a20cbc157ee1d4a03827f2f4

--- src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
+++ src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
@@ -17,8 +17,6 @@ package com.android.systemui.car.wm.scalableui.systemevents;
 
 import static android.car.user.CarUserManager.USER_LIFECYCLE_EVENT_TYPE_SWITCHING;
 import static android.car.user.CarUserManager.USER_LIFECYCLE_EVENT_TYPE_UNLOCKED;
-import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
-import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
 import static com.android.systemui.car.wm.scalableui.systemevents.SystemEventConstants.SYSTEM_BEFORE_USER_SWITCH_EVENT_ID;
 import static com.android.systemui.car.wm.scalableui.systemevents.SystemEventConstants.SYSTEM_ENTER_SUW_EVENT_ID;
@@ -34,8 +32,6 @@ import android.app.KeyguardManager;
 import android.car.user.CarUserManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.TypedArray;
 import android.os.Build;
 import android.os.UserManager;
 import android.util.Log;
@@ -43,16 +39,11 @@ import android.view.Display;
 
 import androidx.annotation.NonNull;
 
-import com.android.car.scalableui.loader.xml.XmlModelLoader;
-import com.android.car.scalableui.manager.ActionManager;
 import com.android.car.scalableui.manager.StateManager;
-import com.android.car.scalableui.model.Action;
 import com.android.car.scalableui.model.Event;
-import com.android.car.scalableui.model.PanelState;
 import com.android.car.scalableui.panel.Panel;
 import com.android.car.scalableui.panel.PanelPool;
 import com.android.systemui.CoreStartable;
-import com.android.systemui.R;
 import com.android.systemui.car.CarDeviceProvisionedController;
 import com.android.systemui.car.CarDeviceProvisionedListener;
 import com.android.systemui.car.CarServiceProvider;
@@ -70,9 +61,7 @@ import com.android.window.flags.Flags;
 
 import dagger.Lazy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.stream.Collectors;
 
@@ -288,29 +277,8 @@ public class SystemEventHandler implements CoreStartable,
     }
 
     @Override
-    public void onConfigChanged(Configuration newConfig) {
+    public void onThemeChanged() {
         PanelPool.getInstance().forEach(Panel::refreshTheme);
-    }
-
-    @Override
-    public void onOrientationChanged(int orientation) {
-        if (mCurrentOrientation != orientation && (ORIENTATION_LANDSCAPE == orientation
-                || ORIENTATION_PORTRAIT == orientation)) {
-            mCurrentOrientation = orientation;
-            XmlModelLoader loader = new XmlModelLoader(mContext);
-
-            TypedArray states = mContext.getResources().obtainTypedArray(R.array.window_states);
-            List<PanelState> panelStateList = new ArrayList<>();
-            for (int i = 0; i < states.length(); i++) {
-                int xmlResId = states.getResourceId(i, 0);
-                PanelState panelState = loader.createPanelState(xmlResId);
-                panelStateList.add(panelState);
-            }
-            StateManager.reloadPanelState(panelStateList);
-
-            List<Action> actions = loader.createActions(R.xml.scalable_ui_actions);
-            ActionManager.setActions(actions);
-        }
     }
 
     private void registerProvisionedStateListener() {

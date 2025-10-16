3c7703f62e6e7f57126d2a3b8bf20dc24626abbc
Alex Stetson <alexstetson@google.com>
2025-10-16 13:46:02 -0700

    Support handling of multi-events
    - Allow for sending multiple events in the same transaction
    - Update SystemEventHandler to use this pattern for event previously
      sent back-to-back
    - Update SystemEventHandler to use legacy keyguard callbacks due to lack
      of compose bouncer support in CarSystemUI
    
    Bug: 452380827
    Test: manual
    Flag: NONE refactor
    Change-Id: I67f2ddbb5d0ebd2100a9034799d0be228bb4e12c

--- src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
+++ src/com/android/systemui/car/wm/scalableui/systemevents/SystemEventHandler.java
@@ -55,16 +55,15 @@ import com.android.systemui.car.wm.scalableui.ScalableUIUtils;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.deviceentry.domain.interactor.DeviceEntryInteractor;
-import com.android.systemui.keyguard.KeyguardWmStateRefactor;
 import com.android.systemui.settings.DisplayTracker;
 import com.android.systemui.settings.UserTracker;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
-import com.android.systemui.util.kotlin.JavaAdapter;
 import com.android.window.flags.Flags;
 
 import dagger.Lazy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.concurrent.Executor;
 import java.util.stream.Collectors;
@@ -87,7 +86,6 @@ public class SystemEventHandler implements CoreStartable,
     private static final boolean DEBUG = Build.IS_DEBUGGABLE;
 
     private final Context mContext;
-    private final JavaAdapter mJavaAdapter;
     private final UserManager mUserManager;
     private final CarServiceProvider mCarServiceProvider;
     private final UserTracker mUserTracker;
@@ -148,7 +146,8 @@ public class SystemEventHandler implements CoreStartable,
                         }
 
                         if (!mIsKeyguardShowing) {
-                            sendUserAuthEvent();
+                            mEventDispatcher.executeEvent(getUserAuthEvent());
+                            mIsUserSwitching = false;
                         }
                     } else {
                         Log.i(TAG, "Ignore system event" + event.getEventType());
@@ -191,12 +190,14 @@ public class SystemEventHandler implements CoreStartable,
     private final UserTracker.Callback mUserTrackerCallback = new UserTracker.Callback() {
         @Override
         public void onBeforeUserSwitching(int newUser) {
-            sendEvent(new Event.Builder(SYSTEM_BEFORE_USER_SWITCH_EVENT_ID));
+            mEventDispatcher.executeEvent(
+                    getEventWithDisplays(new Event.Builder(SYSTEM_BEFORE_USER_SWITCH_EVENT_ID)));
         }
 
         @Override
         public void onUserChanged(int newUser, @NonNull Context userContext) {
-            sendEvent(new Event.Builder(SYSTEM_USER_SWITCH_COMPLETE_EVENT_ID));
+            mEventDispatcher.executeEvent(
+                    getEventWithDisplays(new Event.Builder(SYSTEM_USER_SWITCH_COMPLETE_EVENT_ID)));
         }
     };
 
@@ -206,7 +207,8 @@ public class SystemEventHandler implements CoreStartable,
                 public void onDisplayPowerStateChanged(int displayId, boolean isOn) {
                     if (isOn && mUserManager.isUserUnlocked(mUserTracker.getUserId())
                             && !mIsKeyguardShowing) {
-                        sendUserAuthEvent();
+                        mEventDispatcher.executeEvent(getUserAuthEvent());
+                        mIsUserSwitching = false;
                     }
                 }
             };
@@ -214,7 +216,6 @@ public class SystemEventHandler implements CoreStartable,
     @Inject
     public SystemEventHandler(
             Context context,
-            JavaAdapter javaAdapter,
             UserManager userManager,
             @Background Executor bgExecutor,
             CarServiceProvider carServiceProvider,
@@ -229,7 +230,6 @@ public class SystemEventHandler implements CoreStartable,
             FlagManager flagManager
     ) {
         mContext = context;
-        mJavaAdapter = javaAdapter;
         mUserManager = userManager;
         mBackgroundExecutor = bgExecutor;
         mCarServiceProvider = carServiceProvider;
@@ -255,7 +255,8 @@ public class SystemEventHandler implements CoreStartable,
     private void notifySuwStateEvent() {
         String eventId =
                 mIsUserSetupInProgress ? SYSTEM_ENTER_SUW_EVENT_ID : SYSTEM_EXIT_SUW_EVENT_ID;
-        sendEvent(new Event.Builder(eventId));
+        mEventDispatcher.executeEvent(
+                getEventWithDisplays(new Event.Builder(eventId)));
     }
 
     @Override
@@ -291,18 +292,12 @@ public class SystemEventHandler implements CoreStartable,
 
     private void registerKeyguardStateListener() {
         mIsKeyguardShowing = isKeyguardShowing();
-        if (KeyguardWmStateRefactor.isEnabled()) {
-            mJavaAdapter.alwaysCollectFlow(
-                    mDeviceEntryInteractor.get().isDeviceEntered(),
-                    entered -> keyguardShowingChanged(!entered));
-        } else {
-            mKeyguardStateController.addCallback(new KeyguardStateController.Callback() {
-                @Override
-                public void onKeyguardShowingChanged() {
-                    keyguardShowingChanged(isKeyguardShowing());
-                }
-            });
-        }
+        mKeyguardStateController.addCallback(new KeyguardStateController.Callback() {
+            @Override
+            public void onKeyguardShowingChanged() {
+                keyguardShowingChanged(isKeyguardShowing());
+            }
+        });
     }
 
     private void keyguardShowingChanged(boolean showing) {
@@ -311,21 +306,23 @@ public class SystemEventHandler implements CoreStartable,
         }
         mIsKeyguardShowing = showing;
         if (mIsKeyguardShowing) {
-            sendEvent(new Event.Builder(SYSTEM_KEYGUARD_SHOWN_EVENT_ID));
+            mEventDispatcher.executeEvent(
+                    getEventWithDisplays(new Event.Builder(SYSTEM_KEYGUARD_SHOWN_EVENT_ID)));
         } else {
-            sendEvent(new Event.Builder(SYSTEM_KEYGUARD_HIDDEN_EVENT_ID));
+            ArrayList<Event> eventsToSend = new ArrayList<>();
+            eventsToSend.add(
+                    getEventWithDisplays(new Event.Builder(SYSTEM_KEYGUARD_HIDDEN_EVENT_ID)));
+
             if (mUserManager.isUserUnlocked(mUserTracker.getUserId())) {
-                sendUserAuthEvent();
+                eventsToSend.add(getUserAuthEvent());
+                mIsUserSwitching = false;
             }
+            mEventDispatcher.executeEvents(eventsToSend);
         }
     }
 
     private boolean isKeyguardShowing() {
-        if (KeyguardWmStateRefactor.isEnabled()) {
-            return !mDeviceEntryInteractor.get().isDeviceEntered().getValue();
-        } else {
-            return mKeyguardStateController.isShowing();
-        }
+        return mKeyguardStateController.isShowing();
     }
 
     private boolean shouldResetPanels() {
@@ -333,18 +330,17 @@ public class SystemEventHandler implements CoreStartable,
                 && !mResetCalledForUser;
     }
 
-    private void sendEvent(Event.Builder builder) {
-        mEventDispatcher.executeEvent(builder.addApplicableDisplays(
+    private Event getEventWithDisplays(Event.Builder builder) {
+        return builder.addApplicableDisplays(
                 Arrays.stream(mDisplayTracker.getAllDisplays())
                         .map(Display::getDisplayId)
-                        .collect(Collectors.toList())).build());
+                        .collect(Collectors.toList())).build();
     }
 
-    private void sendUserAuthEvent() {
-        sendEvent(new Event.Builder(
+    private Event getUserAuthEvent() {
+        return getEventWithDisplays(new Event.Builder(
                 SYSTEM_USER_AUTHENTICATED_EVENT_ID)
                 .addToken(SYSTEM_USER_SWITCH_ON_AUTHENTICATED_TOKEN_ID,
                         Boolean.toString(mIsUserSwitching)));
-        mIsUserSwitching = false;
     }
 }

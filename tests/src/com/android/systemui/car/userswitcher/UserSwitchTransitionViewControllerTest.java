15d929420b31b9a6946a0dcbf28ecc7233f495a8
Alex Stetson <alexstetson@google.com>
2025-11-18 10:07:46 -0800

    Refactor user switch transition view logic
    - Give mediator more control of visibility of the overlay while leaving
      the overlay content as part of the controller
    - Change show conditions to be a combination of user switch state and
      user unlocked or keyguard state (depending on if a user is secure)
    
    Bug: 461885644
    Test: manual, atest com.android.systemui.car.userswitcher
    Flag: NONE bugfix
    
    Change-Id: I6f40df60bf9e6f0a425d669477a5a1f15472821f

--- tests/src/com/android/systemui/car/userswitcher/UserSwitchTransitionViewControllerTest.java
+++ tests/src/com/android/systemui/car/userswitcher/UserSwitchTransitionViewControllerTest.java
@@ -16,13 +16,9 @@
 
 package com.android.systemui.car.userswitcher;
 
-import static com.android.systemui.Flags.FLAG_REFACTOR_GET_CURRENT_USER;
-import static com.android.systemui.car.Flags.FLAG_USER_SWITCH_KEYGUARD_SHOWN_TIMEOUT;
-
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
@@ -31,15 +27,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.app.ActivityManager;
-import android.app.KeyguardManager;
 import android.content.pm.UserInfo;
-import android.os.RemoteException;
 import android.os.UserManager;
-import android.platform.test.flag.junit.SetFlagsRule;
 import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
 import android.testing.TestableResources;
-import android.view.IWindowManager;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -54,10 +46,8 @@ import com.android.systemui.util.concurrency.FakeExecutor;
 import com.android.systemui.util.time.FakeSystemClock;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -69,10 +59,7 @@ public class UserSwitchTransitionViewControllerTest extends SysuiTestCase {
     private static final int TEST_USER_1 = 100;
     private static final int TEST_USER_2 = 110;
 
-    @Rule
-    public final SetFlagsRule mSetFlagsRule = new SetFlagsRule();
-
-    private UserSwitchTransitionViewController mCarUserSwitchingDialogController;
+    private UserSwitchTransitionViewController mController;
     private TestableResources mTestableResources;
     private FakeExecutor mExecutor;
     private FakeSystemClock mClock;
@@ -80,13 +67,9 @@ public class UserSwitchTransitionViewControllerTest extends SysuiTestCase {
     @Mock
     private ActivityManager mMockActivityManager;
     @Mock
-    private OverlayViewGlobalStateController mOverlayViewGlobalStateController;
-    @Mock
-    private IWindowManager mWindowManagerService;
-    @Mock
     private UserManager mMockUserManager;
     @Mock
-    private KeyguardManager mKeyguardManager;
+    private OverlayViewGlobalStateController mOverlayViewGlobalStateController;
 
     @Before
     public void setUp() {
@@ -94,40 +77,36 @@ public class UserSwitchTransitionViewControllerTest extends SysuiTestCase {
         mTestableResources = mContext.getOrCreateTestableResources();
         mClock = new FakeSystemClock();
         mExecutor = new FakeExecutor(mClock);
-        when(mKeyguardManager.isDeviceSecure(anyInt())).thenReturn(false);
-        mContext.addMockSystemService(KeyguardManager.class, mKeyguardManager);
-        mCarUserSwitchingDialogController = new UserSwitchTransitionViewController(
+        mController = new UserSwitchTransitionViewController(
                 mContext,
                 mTestableResources.getResources(),
                 mExecutor,
                 mMockActivityManager,
                 mMockUserManager,
-                mWindowManagerService,
                 mOverlayViewGlobalStateController
         );
 
-        mockGetUserInfo(TEST_USER_1);
-        mockGetUserInfo(TEST_USER_2);
+        when(mMockUserManager.getUserInfo(TEST_USER_1))
+                .thenReturn(new UserInfo(TEST_USER_1, "USER_1", /* flags= */ 0));
+        when(mMockUserManager.getUserInfo(TEST_USER_2))
+                .thenReturn(new UserInfo(TEST_USER_2, "USER_2", /* flags= */ 0));
         mockGlobalShowView();
         mViewGroup = (ViewGroup) LayoutInflater.from(mContext).inflate(
                 R.layout.sysui_overlay_window, /* root= */ null);
-        mCarUserSwitchingDialogController.inflate(mViewGroup);
+        mController.inflate(mViewGroup);
     }
 
     @Test
-    public void onHandleShow_newUserSelected_showsDialog() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+    public void showSwitchingUI_newUserSelected_showsDialog() {
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
 
-        verify(mOverlayViewGlobalStateController).showView(eq(mCarUserSwitchingDialogController),
-                any());
+        verify(mOverlayViewGlobalStateController).showView(eq(mController), any());
     }
 
     @Test
-    public void onHandleShow_showsDefaultLoadingMessage() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+    public void showSwitchingUI_showsDefaultLoadingMessage() {
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
 
         TextView textView = mViewGroup.findViewById(R.id.user_loading);
@@ -136,12 +115,11 @@ public class UserSwitchTransitionViewControllerTest extends SysuiTestCase {
     }
 
     @Test
-    public void onHandleShow_showsUserSwitchingMessage() {
+    public void showSwitchingUI_showsUserSwitchingMessage() {
         String message = "Hello world!";
         when(mMockActivityManager.getSwitchingFromUserMessage()).thenReturn(message);
 
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
 
         TextView textView = mViewGroup.findViewById(R.id.user_loading);
@@ -149,182 +127,77 @@ public class UserSwitchTransitionViewControllerTest extends SysuiTestCase {
     }
 
     @Test
-    public void onHandleShow_alreadyShowing_ignoresRequest() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+    public void showSwitchingUI_alreadyShowing_ignoresRequest() {
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_2);
-        mExecutor.advanceClockToLast();
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_2);
         mExecutor.runAllReady();
 
         // Verify that the request was processed only once.
-        verify(mOverlayViewGlobalStateController).showView(eq(mCarUserSwitchingDialogController),
-                any());
+        verify(mOverlayViewGlobalStateController).showView(eq(mController), any());
     }
 
     @Test
-    public void onHandleShow_sameUserSelected_ignoresRequest() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+    public void showSwitchingUI_sameUserSelected_ignoresRequest() {
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleHide();
-        mExecutor.advanceClockToLast();
+        mController.hideSwitchingUI();
         mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
 
         // Verify that the request was processed only once.
-        verify(mOverlayViewGlobalStateController).showView(eq(mCarUserSwitchingDialogController),
-                any());
+        verify(mOverlayViewGlobalStateController).showView(eq(mController), any());
     }
 
     @Test
-    public void onHandleShow_noUserRefactor_setsWMState() throws RemoteException {
-        mSetFlagsRule.disableFlags(FLAG_REFACTOR_GET_CURRENT_USER);
-
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+    public void hideSwitchingUI_currentlyShowing_hidesDialog() {
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
-
-        verify(mWindowManagerService).setSwitchingUser(true);
-        verify(mWindowManagerService).lockNow(null);
-    }
-
-    @Test
-    public void onHandleShow_userRefactor_setsWMState() throws RemoteException {
-        mSetFlagsRule.enableFlags(FLAG_REFACTOR_GET_CURRENT_USER);
-
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-
-        verify(mWindowManagerService).setSwitchingUser(true);
-        verify(mWindowManagerService, never()).lockNow(null);
-    }
-
-    @Test
-    public void handleSwitching_noUserRefactor_doNothing() throws RemoteException {
-        mSetFlagsRule.disableFlags(FLAG_REFACTOR_GET_CURRENT_USER);
-
-        mCarUserSwitchingDialogController.handleSwitching(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-
-        verify(mWindowManagerService, never()).lockNow(null);
-    }
-
-    @Test
-    public void handleSwitching_userRefactor_userNotSecure_doNothing() throws RemoteException {
-        mSetFlagsRule.enableFlags(FLAG_REFACTOR_GET_CURRENT_USER);
-
-        mCarUserSwitchingDialogController.handleSwitching(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-
-        verify(mWindowManagerService, never()).lockNow(null);
-    }
-
-    @Test
-    public void handleSwitching_userRefactor_userSecure_setsWMState() throws RemoteException {
-        mSetFlagsRule.enableFlags(FLAG_REFACTOR_GET_CURRENT_USER);
-        when(mKeyguardManager.isDeviceSecure(anyInt())).thenReturn(true);
-
-        mCarUserSwitchingDialogController.handleSwitching(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-
-        verify(mWindowManagerService).lockNow(null);
-    }
-
-    @Test
-    public void onHide_currentlyShowing_hidesDialog() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleHide();
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-
-        verify(mOverlayViewGlobalStateController).hideView(eq(mCarUserSwitchingDialogController),
-                any());
-    }
-
-    @Test
-    public void onHide_notShowing_ignoresRequest() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleHide();
-        mExecutor.advanceClockToLast();
-        mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleHide();
-        mExecutor.advanceClockToLast();
+        mController.hideSwitchingUI();
         mExecutor.runAllReady();
 
         // Verify that the request was processed only once.
-        verify(mOverlayViewGlobalStateController).hideView(eq(mCarUserSwitchingDialogController),
-                any());
+        verify(mOverlayViewGlobalStateController).hideView(eq(mController), any());
     }
 
     @Test
     public void onWindowShownTimeoutPassed_viewNotHidden_hidesUserSwitchTransitionView() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
         reset(mOverlayViewGlobalStateController);
 
-        mClock.advanceTime(mCarUserSwitchingDialogController.getWindowShownTimeoutMs() + 10);
+        mClock.advanceTime(mController.getWindowShownTimeoutMs() + 10);
+        mExecutor.runAllReady();
 
-        verify(mOverlayViewGlobalStateController).hideView(
-                eq(mCarUserSwitchingDialogController), any());
+        verify(mOverlayViewGlobalStateController).hideView(eq(mController), any());
     }
 
     @Test
     public void onWindowShownTimeoutPassed_viewHidden_doesNotHideUserSwitchTransitionViewAgain() {
-        mCarUserSwitchingDialogController.handleShow(/* newUserId= */ TEST_USER_1);
-        mExecutor.advanceClockToLast();
+        mController.showSwitchingUI(/* newUserId= */ TEST_USER_1);
         mExecutor.runAllReady();
-        mCarUserSwitchingDialogController.handleHide();
-        mExecutor.advanceClockToLast();
+        mController.hideSwitchingUI();
         mExecutor.runAllReady();
         reset(mOverlayViewGlobalStateController);
 
-        mClock.advanceTime(mCarUserSwitchingDialogController.getWindowShownTimeoutMs() + 10);
+        mClock.advanceTime(mController.getWindowShownTimeoutMs() + 10);
+        mExecutor.runAllReady();
 
-        verify(mOverlayViewGlobalStateController, never()).hideView(
-                eq(mCarUserSwitchingDialogController), any());
+        verify(mOverlayViewGlobalStateController, never()).hideView(eq(mController), any());
     }
 
     @Test
-    public void setupKeyguardShownTimeout_keyguardLocked_doNothing() throws RemoteException {
-        mSetFlagsRule.enableFlags(FLAG_USER_SWITCH_KEYGUARD_SHOWN_TIMEOUT);
-        when(mWindowManagerService.isKeyguardLocked()).thenReturn(true);
+    public void onWindowShownTimeoutPassed_skipTimeoutSet_doesNotHideView() {
+        mController.setShouldSkipTimeout(true);
+        mController.showSwitchingUI(TEST_USER_1);
+        mExecutor.runAllReady();
+        reset(mOverlayViewGlobalStateController);
 
-        mCarUserSwitchingDialogController.setupKeyguardShownTimeout();
+        mClock.advanceTime(mController.getWindowShownTimeoutMs() + 10);
+        mExecutor.runAllReady();
 
-        verify(mKeyguardManager, never()).addKeyguardLockedStateListener(any(), any());
-    }
-
-    @Test
-    public void setupKeyguardShownTimeout_keyguardNotLocked_addTimeout() throws RemoteException {
-        mSetFlagsRule.enableFlags(FLAG_USER_SWITCH_KEYGUARD_SHOWN_TIMEOUT);
-        when(mWindowManagerService.isKeyguardLocked()).thenReturn(false);
-        ArgumentCaptor<KeyguardManager.KeyguardLockedStateListener> lockedStateListenerCaptor =
-                ArgumentCaptor.forClass(KeyguardManager.KeyguardLockedStateListener.class);
-
-        mCarUserSwitchingDialogController.setupKeyguardShownTimeout();
-
-        verify(mKeyguardManager).addKeyguardLockedStateListener(any(),
-                lockedStateListenerCaptor.capture());
-        assertThat(mExecutor.numPending()).isGreaterThan(0);
-        lockedStateListenerCaptor.getValue().onKeyguardLockedStateChanged(true);
-        verify(mKeyguardManager).removeKeyguardLockedStateListener(any());
-    }
-
-    private void mockGetUserInfo(int userId) {
-        when(mMockUserManager.getUserInfo(userId))
-                .thenReturn(new UserInfo(userId, "USER_" + userId, /* flags= */ 0));
+        verify(mOverlayViewGlobalStateController, never()).hideView(eq(mController), any());
     }
 
     private void mockGlobalShowView() {

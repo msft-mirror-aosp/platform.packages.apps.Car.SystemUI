39a09dc185d0bf14d7739facca52977ba9fd3e0b
Alex Stetson <alexstetson@google.com>
2025-12-05 16:16:05 -0800

    Apply focus for multi-events and when unchanging
    - Use the PanelTransaction events to detect open events, allowing for
      handling of multi-events.
    - Re-apply focus to the current focused task when it shouldn't change to
      prevent the system from taking focus away.
    
    Bug: 459702660
    Test: manual, atest CtsWidgetTestCases
    Flag: NONE bugfix
    Change-Id: Ibd21f92ac1b32efb093dfe5875333e0e0ef7209e

--- src/com/android/systemui/car/wm/scalableui/PanelAutoTaskStackTransitionHandlerDelegate.java
+++ src/com/android/systemui/car/wm/scalableui/PanelAutoTaskStackTransitionHandlerDelegate.java
@@ -140,7 +140,7 @@ public class PanelAutoTaskStackTransitionHandlerDelegate implements
         PanelTransaction panelTransaction = EventDispatcher.getTransaction(event);
         AutoTaskStackTransaction wct =
                 mPanelTransitionCoordinator.createAutoTaskStackTransaction(transition,
-                        panelTransaction, event);
+                        panelTransaction);
         mPanelTransitionCoordinator.resetUnpreparedDecorPanel(panelTransaction);
         if (DEBUG) {
             Log.d(TAG, "handleRequest: COMPLETED " + wct);

146820ad27669d80d6db2f4f5694f9c441d5c45d
SWEAP Agent <sweap-agent-service@google.com>
2026-03-05 13:03:26 -0800

    [Flag Removal] Remove flag scalable_ui_task_focus of package com.android.car.scalableui (1/2)
    Bug: 485386654
    Fixes: 485386654
    Test: All TreeHugger presubmit checks passed.
    Flag: EXEMPT FLAG_REMOVAL
    OFF_PEAK_PRESUBMIT: true
    auto-generated-cl: flag-removal-agent
    Change-Id: Ie0a0305a536d73d3543f2200f9e4450c23b5771e

--- src/com/android/systemui/car/wm/scalableui/PanelTransitionCoordinator.java
+++ src/com/android/systemui/car/wm/scalableui/PanelTransitionCoordinator.java
@@ -961,9 +961,7 @@ public class PanelTransitionCoordinator {
             applyAutoTaskStackState(autoTaskStackTransaction, panelId, /* toVariant= */ null);
         }
 
-        if (mFlagManager.isEnabled(Flag.ScalableUiTaskFocus)) {
-            calculateFocusedTaskStack(panelTransaction, autoTaskStackTransaction);
-        }
+        calculateFocusedTaskStack(panelTransaction, autoTaskStackTransaction);
 
         return autoTaskStackTransaction;
     }
